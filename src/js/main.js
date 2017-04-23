ef2ebcf60548ef0eaa008ea25099369cde144014
Vitaliy Fomenko <vitaliy.fomenko@ukr.net>
2017-04-24 01:05:30 +0300

    create steps level for trip form

--- src/js/main.js
+++ src/js/main.js
@@ -4,6 +4,23 @@ $( function() {
   $( "#depart_date" ).datepicker();
   $( "#depart_time" ).selectmenu();
   $( "#sur_place" ).selectmenu();
+  $( "#step-part-time" ).selectmenu();
+  $( "#step-part-time2" ).selectmenu();
+  $('.step-parts').hide();
+
+  var switchValue = false;
+
+  $('#step-switcher').on('click', function(){
+    $('#step-switcher').toggleClass('active-switch');
+    $('.step-parts').toggle();
+    switchValue = ! switchValue;
+    if(switchValue){
+      $('#step-switcher').text('ON');
+    }else{
+      $('#step-switcher').text('OFF');
+    }
+
+  });
 } );
 
 //--------------------------------------------
